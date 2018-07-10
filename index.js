0cf08139e9b916e83bb91c0fec524469653f1026
Sankalp Johri <sankalp.johri@iiitb.org>
2018-07-10 10:07:45 +0530

    Added comments

--- index.js
+++ index.js
@@ -10,16 +10,22 @@ import englishToBrailleLiteralSet from './english-to-braille.js';
 
 const englishToBrailleMap = new Map();
 
+// Convert binding set to map.
 englishToBrailleLiteralSet.forEach((element) => {
   englishToBrailleMap.set(element[0], element[1]);
 });
 
 function convertEnglishToBraille() {
+  // Get input from the text field.
   const inputText = document.getElementById('sourceLangText').value;
   const outputText = [];
-  for (let i = 0; i < inputText.length; i += 1) {
+  // Set length of the input text to a variable for optimising the code.
+  const len = inputText.length;
+  // Run a loop to go over the input string andd find it's corresponding character in braille.
+  for (let i = 0; i < len; i += 1) {
     outputText.push(englishToBrailleMap.get(inputText.charAt(i)));
   }
+  // Set the converted text as inner html of the output element.
   document.getElementById('targetLangText').innerHTML = outputText.join('');
 }
 
